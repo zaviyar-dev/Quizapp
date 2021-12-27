4067e471916ff54783a71f6eee7d92c9411e12c8
zaviyar-dev <83871709+zaviyar-dev@users.noreply.github.com>
2021-12-27 19:50:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,223 @@
+let questionDb = [
+    `<ul>
+        <h4>HTML is for</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="correct" class="opt">
+            <label for="Q1">Creating Website Structure</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">Creating Website Designing</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q3" value="wrong" class="opt">
+            <label for="Q3">Website Behaviour</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All of these</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>React is a</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">JavaScript Framework</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript Library for website developement</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">JavaScript Library for website designing</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">Both b and c</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+    `<ul>
+        <h4>Which statement is correct</h4>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q1" value="wrong" class="opt">
+            <label for="Q1">Bootstrap is framework of HTML CSS</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q2" value="wrong" class="opt">
+            <label for="Q2">JavaScript only use in website developement</label>
+        </li>
+        <li class="QnA">
+            <input type="radio" name="ans" id="Q3" value="correct" class="opt">
+            <label for="Q3">Node.js support non-blocking I/O model</label>
+        </li>
+        <li class="QnA" onClick = "liBackgroundColorChanger()">
+            <input type="radio" name="ans" id="Q4" value="wrong" class="opt">
+            <label for="Q4">All statements are correct</label>
+        </li>
+    </ul>`,
+]
+
+let answer = 'correct';
+let score = 0;
+let temp = 0
+let input = document.getElementsByTagName('li')
+const submit = () => {
+    let options = document.getElementsByClassName('opt')
+    Array.from(options).forEach((entry) => {
+        if (entry.checked) {
+            if (answer === entry.value) {
+                score += 1;
+            } else {
+                score = score;
+            }
+        } else {
+            console.log('nothing');
+        }
+    })
+    temp += 1
+    updateQuestion(temp)
+    // console.log(score);
+}
+const updateQuestion = (count)=>{
+    if (count <= questionDb.length - 1) {
+        document.getElementsByClassName('container')[0].innerHTML = questionDb[count];
+    } else {
+        document.getElementsByClassName('main-container')[0].style.display = 'none'
+        alert(`Thansk for your time.You Scored ${score}`)
+    }
+}
+updateQuestion(temp)
